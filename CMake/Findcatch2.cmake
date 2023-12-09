6467496b7dda7e66e8a7142780834679e5bbdb35
Tatsuya Shiozawa <shio.tatsu99@gmail.com>
2023-12-09 14:22:54 +0900

    Add CLAP format.

--- CMake/Findcatch2.cmake
+++ CMake/Findcatch2.cmake
@@ -1,4 +1,4 @@
 include(CPM)
 
-CPMAddPackage("gh:catchorg/Catch2#devel")
+CPMAddPackage("gh:catchorg/Catch2#v3.4.0")
 include(${Catch2_SOURCE_DIR}/extras/Catch.cmake)
--- CMake/Findclap-juce-extensions.cmake
+++ CMake/Findclap-juce-extensions.cmake
@@ -2,6 +2,6 @@
 #https://github.com/TheLartians/CPM.cmake
 include(CPM)
 
-#Fetching fmt from git
+#Fetching clap-juce-extensions from git
 #IF you want to instead point it to a local version, you can invoke CMake with
-CPMAddPackage("gh:fmtlib/fmt#master")
+CPMAddPackage("gh:free-audio/clap-juce-extensions#54d83e121545490fdbb2d3dec3ba0c1da6005dde")
